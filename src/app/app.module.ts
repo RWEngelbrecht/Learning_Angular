2b799f16fb1bf783f7d2291bd0ad21be83de4316
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2020-02-10 10:50:38 +0200

    import http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { ToDoItemComponent } from './components/to-do-item/to-do-item.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    ToDoItemComponent
+    ToDoItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
