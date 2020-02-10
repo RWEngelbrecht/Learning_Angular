8e171e37eec0aafac005f520a099a35f0a429584
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2020-02-10 10:56:23 +0200

    deleteTodo calls httpClient delete() method. removes todo from server

--- src/app/services/to-do.service.ts
+++ src/app/services/to-do.service.ts
@@ -17,14 +17,21 @@ export class ToDoService {
 
   todosUrl = 'https://jsonplaceholder.typicode.com/todos';
   todosLimit = '?_limit=5';
+
   constructor(private http: HttpClient) { }
 
   getTodos(): Observable<ToDo[]> {
+    // return array of jsonplaceholder todo items
     return this.http.get<ToDo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
   toggleCompleted(todo: ToDo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`;
     return this.http.put(url, todo, httpOptions);
+  }
+
+  deleteTodo(todo: ToDo): Observable<ToDo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<ToDo>(url, httpOptions);
   }
 }
